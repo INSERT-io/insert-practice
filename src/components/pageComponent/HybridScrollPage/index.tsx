c8967fc4da513c0891d2d8d571bae150639e43f3
INSERT-io <isomanatsuki@team-lab.com>
2025-03-14 12:51:52 +0900

    コメントの追加

--- src/components/pageComponent/HybridScrollPage/index.tsx
+++ src/components/pageComponent/HybridScrollPage/index.tsx
@@ -5,6 +5,7 @@ import { Presenter } from "./presenter";
 export const HybridScrollPage: React.FC = () => {
   useEffect(() => {
     const transform = (section: Element) => {
+      // scroll要素のtopの座標を取得
       const offsetTop = section.parentElement?.offsetTop;
 
       const scrollSection = section.querySelector(
@@ -12,15 +13,19 @@ export const HybridScrollPage: React.FC = () => {
       ) as HTMLElement | null;
 
       if (scrollSection === null || offsetTop === undefined) return;
+      console.log(offsetTop);
 
       let percentage =
         ((window.scrollY - offsetTop) / window.innerHeight) * 100;
 
+      // 0 ~ 300vwの間でスクロールを制限
       percentage = percentage < 0 ? 0 : percentage > 300 ? 300 : percentage;
 
-      scrollSection.style.transform = `translate3d(${-percentage}vw, 0, 0)`;
+      // 縦にスクロールした分だけ要素を横に移動させる
+      scrollSection.style.transform = `translateX(${-percentage}vw)`;
     };
 
+    // 全てのstickyWrapperを取得
     const stickySections = [
       ...document.querySelectorAll(`.${styles.stickyWrapper}`),
     ];
